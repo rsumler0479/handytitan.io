253b71ee756d11546df0c56e7a66968adf6260eb
Jack Reid <rsumler0479@gmail.com>
2024-10-15 19:14:49 -0400

    landing page

---
+++ assets/js/landing.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("appointment-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Appointment submitted successfully!");
+  });
